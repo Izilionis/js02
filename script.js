e094ccd97266825586390952d727597fbbfb0dde
Izilionis <148207556+Izilionis@users.noreply.github.com>
2023-10-19 20:04:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//Array (masyvai)
+
+var name0 = "Jon Snow"
+var name1 = "Cersei Lannisteri"
+var name2 = "Daenerys Targaryen"
+var name3 = "Theon Greyjoy"
+var name4 = "Tyrion Lannister"
+var name5 = "Arya Stark"
+
+console.log(name0 + " ir " + name5)
+
+vardas = prompt("Įveskite skaičių nuo 1 iki 6")
+
+console.log(vardas)
+
+// var  vardas1 = name0
+// var  vardas2 = name0
+// var  vardas3 = name0
+// var  vardas4 = name0
+// var  vardas5 = name0
+// var  vardas6 = name0
+
+// console.log(vardas)
+
